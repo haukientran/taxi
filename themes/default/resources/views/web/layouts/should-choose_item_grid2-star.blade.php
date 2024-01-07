ff546995b00df57ceac798cd603ee8c191f43a25
haukientran <70125147+haukientran@users.noreply.github.com>
2024-01-07 15:41:07 +0700

    Fix grid 4 && fix danh gia sao

--- themes/default/resources/views/web/layouts/should-choose_item_grid2-star.blade.php
+++ themes/default/resources/views/web/layouts/should-choose_item_grid2-star.blade.php
@@ -1,7 +1,7 @@
 <div class="should-choose-item flex-center-between">
     <div class="should-choose-item__thumbnail">
         @include('Default::general.components.image', [
-            'src' => resizeWImage($setting_home['should_choose_grid2']['image'][$key] ?? '', 'w100'),
+            'src' => resizeWImage($setting_home['should_choose_grid2_star']['image'][$key] ?? '', 'w100'),
             'width' => '80',
             'height' => '80',
             "lazy"   => true,
@@ -17,6 +17,6 @@
             @endfor
         @endif
             <h3 class="should-choose-item__title f-w-b">{{ $title ?? '' }}</h3>
-        <div class="should-choose-item__desc four-lines-ellipsis">{{ isset($setting_home['should_choose_grid2']['description'][$key]) ? $setting_home['should_choose_grid2']['description'][$key] : '' }}</div>
+        <div class="should-choose-item__desc four-lines-ellipsis">{{ isset($setting_home['should_choose_grid2_star']['description'][$key]) ? $setting_home['should_choose_grid2_star']['description'][$key] : '' }}</div>
     </div>
 </div>
